311c7c3d93a51ff08f201b81b5f6d3979c00d1cc
Evgenii <voznukzhenia@gmail.com>
2020-12-18 18:22:25 +0200

    init

---
+++ src/views/Login.vue
@@ -0,0 +1,124 @@
+<template>
+  <div id="login">
+    <PasswordReset v-if="showPasswordReset" @close="togglePasswordReset()"></PasswordReset>
+    <section>
+      <div class="col1">
+        <h1>Vue-app</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nulla, optio
+          suscipit, corporis facere eius labore culpa in modi saepe harum, neque
+          perferendis. Aliquid veritatis deserunt libero repellat odio
+          architecto assumenda?
+        </p>
+      </div>
+      <div :class="{ 'signup-form': !showLoginForm }" class="col2">
+        <form @submit.prevent v-if="showLoginForm">
+          <h1>Welcome Back</h1>
+          <div>
+            <label for="email1">Email</label>
+            <input
+              v-model.trim="loginForm.email"
+              type="text"
+              placeholder="you@email.com"
+              id="email1"
+            />
+          </div>
+          <div>
+            <label for="password1">Password</label>
+            <input
+              v-model.trim="loginForm.password"
+              type="password"
+              placeholder="******"
+              id="password1"
+            />
+          </div>
+          <button class="button" @click="login()">Log In</button>
+          <div class="extras">
+            <a @click="togglePasswordReset()">Forgot Password</a>
+            <a @click="toggleForm()">Create an Account</a>
+          </div>
+        </form>
+        <form @submit.prevent v-else>
+          <h1>Get Started</h1>
+          <div>
+            <label for="name">Name</label>
+            <input
+              v-model.trim="signupForm.name"
+              type="text"
+              placeholder="Savvy Apps"
+              id="name"
+            />
+          </div>
+          <div>
+            <label for="email2">Email</label>
+            <input
+              v-model.trim="signupForm.email"
+              type="text"
+              placeholder="you@email.com"
+              id="email2"
+            />
+          </div>
+          <div>
+            <label for="password2">Password</label>
+            <input
+              v-model.trim="signupForm.password"
+              type="password"
+              placeholder="min 6 characters"
+              id="password2"
+            />
+          </div>
+          <button @click="signup()" class="button">Sign Up</button>
+          <div class="extras">
+            <a @click="toggleForm()">Back to Log In</a>
+          </div>
+        </form>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+import PasswordReset from '../components/PasswordReset.vue';
+
+export default {
+  components: {
+    PasswordReset,
+  },
+  data() {
+    return {
+      loginForm: {
+        email: '',
+        password: '',
+      },
+      signupForm: {
+        name: '',
+        email: '',
+        password: '',
+      },
+      showLoginForm: true,
+      showPasswordReset: false,
+    };
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('login', {
+        email: this.loginForm.email,
+        password: this.loginForm.password,
+      });
+    },
+    signup() {
+      this.$store.dispatch('signup', {
+        email: this.signupForm.email,
+        password: this.signupForm.password,
+        name: this.signupForm.name,
+      });
+    },
+    toggleForm() {
+      this.showLoginForm = !this.showLoginForm;
+    },
+    togglePasswordReset() {
+      this.showPasswordReset = !this.showPasswordReset;
+    },
+  },
+};
+</script>
---
+++ src/views/Settings.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="Settings">
+    <h1>Settings</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {
+  },
+};
+</script>
